23b6d463cbe3077d430ceb3f2157d44d95f8d733
Tiara Dwi Meilinda A <tiaradwi013@gmail.com>
2022-10-24 11:21:01 +0700

    ìnitial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "hello world! <br>";
+
+    $nama = "Tiara";
+    $umur = 21;
+    
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaKakak = "kyungsoo";
+    $umurKakak = 29;
+
+    echo "Nama kakak saya <strong>$namaKakak</strong>, kakak saya berusia $umurKakak tahun. <br>";
+
+    $selisihUmur = $umur - $umurKakak ;
+    echo "selisih usia saya dan kakak saya adalah $selisihUmur tahun"; 
+?>
+
